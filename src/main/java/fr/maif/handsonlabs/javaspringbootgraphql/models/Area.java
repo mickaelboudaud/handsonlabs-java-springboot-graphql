3d12ace54aa399373d041c07f56b47eb103155fa
Mickaël Boudaud <mickael.boudaud@gmail.com>
2019-09-12 10:49:09 +0200

    Area renaming

--- src/main/java/fr/maif/handsonlabs/javaspringbootgraphql/models/Area.java
+++ src/main/java/fr/maif/handsonlabs/javaspringbootgraphql/models/Area.java
@@ -1,7 +1,5 @@
 package fr.maif.handsonlabs.javaspringbootgraphql.models;
 
-import com.fasterxml.jackson.annotation.JsonValue;
-
 /**
  * Country Area.
  *
@@ -9,11 +7,21 @@ import com.fasterxml.jackson.annotation.JsonValue;
  * https://fr.vikidia.org/wiki/Liste_des_pays_par_superficie
  */
 public enum Area {
-    F0_T10000,
-    F10000_T50000,
-    F50000_T100000,
-    F100000_T500000,
-    F500000_T1000000,
-    F1000000_T5000000,
-    F5000000;
+    F0_T10000("De 0 à 10000 Km²"),
+    F10000_T50000("De 10000 à 50000 Km²"),
+    F50000_T100000("De 50000 à 100000 Km²"),
+    F100000_T500000("De 100000 à 500000 Km²"),
+    F500000_T1000000("De 500000 à 1000000 Km²"),
+    F1000000_T5000000("De 1000000 à 5000000 Km²"),
+    F5000000("Plus de 5000000 de Km²");
+
+    private String label;
+
+    Area(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
 }
